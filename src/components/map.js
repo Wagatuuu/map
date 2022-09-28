5089aa867ae0d4f964abf2dc5c8962627fbc816c
Sceptre <mbuguag026@gmail.com>
2022-09-28 18:54:38 +0300

    updates

--- src/components/map.js
+++ src/components/map.js
@@ -1,9 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import { MapContainer, TileLayer, Marker} from 'react-leaflet';
+import {useNavigate} from 'react-router-dom';
 import {Modal} from '@mui/material';
 import Upload from './upload';
 
+const token = localStorage.getItem('token');
+
 function Map(){
+
+    let navigate = useNavigate();
 
     const [location, setLocation] = useState('');
     const [noiseinfo, setNoiseinfo] = useState([]);
@@ -21,6 +26,10 @@ function Map(){
         }
         fetchData()
     },[]);
+
+    if(!token){
+        return navigate('/', {replace: true})
+    };
 
     const place = noiseinfo.find(data => {
         return data.place === location
